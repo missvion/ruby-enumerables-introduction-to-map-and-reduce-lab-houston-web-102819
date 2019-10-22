7b12ba9be3e7dbe4ed6a96e8cf97c40f66bd8ec8
Vivian Tran <viviantran94@gmail.com>
2019-10-22 00:53:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,5 +12,13 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
+  final_array = []
+  index = 0
   
+  while index < source_array.length do
+    final_array << source_array[index]  
+    index += 1 
+  end
+  final_array
+end    
     
